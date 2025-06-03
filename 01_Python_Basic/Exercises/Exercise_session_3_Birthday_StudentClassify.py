feb6c79297bab10610638df2ed9ddfaf721485e8
AnhTuTruong <truonganhtuhdvn@gmail.com>
2025-06-04 04:10:42 +0700

    exercise 3 done

--- 01_Python_Basic/Exercises/Exercise_session_3_Birthday_StudentClassify.py
+++ 01_Python_Basic/Exercises/Exercise_session_3_Birthday_StudentClassify.py
@@ -9,6 +9,58 @@ Q3: what is the weekday of the last day of your birthmonth?
 Q4: is your birth year a leap year? If not, what is the closest one to your birthyear?
 Q5: what is the timedelta counted in days from your birthday to present?
 '''
+######################################################
+#Tu's answer:
+
+#Q1
+from datetime import datetime
+from datetime import date
+import calendar
+
+x = input("Type your birthday in DD-MM-YYYY:")
+
+print(x)
+
+b = datetime.strptime(x, "%d-%m-%Y")
+
+print(b)
+
+#Q2
+print(b.strftime("%A"))
+
+#Q3
+c = date(b.year, b.month, calendar.monthrange(b.year, b.month)[1])
+
+print(c.strftime("%A"))
+
+#Q4
+year = b.year
+
+if calendar.isleap(year):
+    print(f"The year {year} is a leap year")
+else:
+    print(f"The year {year} is not a leap year")
+
+    for i in range(1, 100):
+        if calendar.isleap(year - i):
+            prev = year - i
+            break
+    for i in range(1, 100):
+        if calendar.isleap(year + i):
+            next = year + i
+            break
+    if (year - prev) <= (next - year):
+        closest = prev
+    else:
+        closest = next
+
+    print(f"The closest year to your birthyear: {closest}")
+
+
+#Q5
+today = datetime.today()
+e = today - b
+print(e.days)
 
 ##############################################
 
@@ -28,3 +80,50 @@ Use try-except to ensure the right datatype of the input (float or integer)
 Use try-assert to ensure the right range of the input (score >= 0, and score <= 100, not negative)
 Use while loop to ask if the user wants to continue, set the break condition if the does not want to.
 '''
+
+######################################################
+#Tu's answer:
+
+
+try:
+    score = float(input("Input your score:"))
+    assert 0 <= score <= 100
+except:
+    print(">>> Error: Your input is not numeric and needs to be in range between 0 to 100")
+
+
+
+
+
+while True:
+    try:
+        score = float(input("Input your score:"))
+        assert 0 <= score <= 100
+    except ValueError:
+        print(">>> Error: Your input is not numeric!!!!")
+        continue
+    except AssertionError:
+        print("Your score needs to be in range between 0 to 100")
+        continue
+    else:
+        print("Your input is valid")
+        break
+
+
+
+if score < 50:
+    r = "Weak"
+elif score < 60:
+    r = "Poor"
+elif score < 70:
+    r = "Satisfactory"
+elif score < 85:
+    r = "Good"
+else:
+    r = "Excellent"
+
+print(f"Your score is: {score} -> {r}")
+
+
+
+
